c579bbd340ce23bcdf1b25ef573789e2eda40783
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-03-31 05:15:23 +0700

    fix

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 #include <unistd.h>
 #include "pwm_servo.h"
 
-#define MIN_DUTY_CYCLE 5   // Duty cycle for 0 degrees (approx. 500μs pulse width at 1000Hz)
-#define MAX_DUTY_CYCLE 25  // Duty cycle for 180 degrees (approx. 2500μs pulse width at 1000Hz)
+#define MIN_DUTY_CYCLE 500   // Duty cycle for 0 degrees (approx. 500μs pulse width at 1000Hz)
+#define MAX_DUTY_CYCLE 2500  // Duty cycle for 180 degrees (approx. 2500μs pulse width at 1000Hz)
 #define DELAY_US 1000000   // Delay between movements in microseconds (1 second)
 
 int main() {
@@ -11,7 +11,7 @@ int main() {
     PCA9685_init();
 
     // Set PWM frequency to 1000Hz (within the specified 50-330Hz range)
-    set_pwm_freq(1000);
+    set_pwm_freq(50);
 
     while (1) {
         // Move servo to 0 degrees (500μs pulse width)
