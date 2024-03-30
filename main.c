7a6e6b3f6536841e594f6b94b92f8ca9149242fa
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-03-31 03:29:36 +0700

    fix:

--- main.c
+++ main.c
@@ -2,18 +2,18 @@
 
 int i2c_fd;
 
-int main(void) 
-{
+int main(void) {
     if (open_i2_device() != 0) {
         return -1;
     }
 
-    if (init_pca9685() != 0 ){
+    if (init_pca9685() != 0) {
+        close(i2c_fd);  // Close I2C device before exiting
         return -1;
     }
 
-    set_servo_position(0, 1500);
+    set_servo_position(0, 1500);  // Set servo on channel 0 to midpoint
 
-    close(i2c_fd);
+    close(i2c_fd);  // Close I2C device after use
     return 0;
 }
