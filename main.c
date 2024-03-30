9d5769f914b5999ce3c5d9cdb16f2ed5d9bb4707
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-03-31 04:37:35 +0700

    fix

--- main.c
+++ main.c
@@ -3,19 +3,21 @@
 int i2c_fd;
 
 int main(void) {
-
+        // Initialize PCA9685 (void function, no return value)
     PCA9685_init();
 
+    // Set PWM frequency to 1000Hz (void function, no return value)
     set_pwm_freq(1000);
 
-    set_pwm_duty(1, 1024.0);
+    // Set PWM duty cycle for channel 1 to 50% (void function, no return value)
+    set_pwm_duty(1, 2048);
 
-    int pwmValue = get_pwm(0);
-
-
+    // Get current PWM value for channel 1
+    int pwmValue = get_pwm(1);
     printf("Current PWM value for channel 1: %d\n", pwmValue);
 
     // Close I2C device
     close(i2c_fd);
+
     return 0;
 }
