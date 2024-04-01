e33ca08d04c0d3e376ead7b17b06332efbd08b6b
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-02 05:44:22 +0700

    fix

--- main.c
+++ main.c
@@ -7,21 +7,16 @@ int main(void)
     PCA9685_init();
 
     while(1){
-        set_pwm_angle(2, 0, 50);
-        int pwm_value = get_pwm(2);
-        printf("pwm: %d", pwm_value);
+        set_pwm_angle(2, 0, 60);
 
         sleep(1);
 
-        set_pwm_angle(2, 90, 50);
-        pwm_value = get_pwm(2);
-        printf("pwm: %d", pwm_value);
+        set_pwm_angle(2, 90, 60);
 
         sleep(1);
 
-        set_pwm_angle(2, 180, 50);
-        pwm_value = get_pwm(2);
-        printf("pwm: %d", pwm_value);
+        set_pwm_angle(2, 180, 60);
+
 
         sleep(1);
     }
