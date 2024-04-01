1c78487ade6bddd0d338685898e14e9dbf0e3abf
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-02 04:50:21 +0700

    fix

--- main.c
+++ main.c
@@ -8,15 +8,15 @@ int main(void)
 
 
     while(1){
-        set_pwm_angle(1, 0, 100);
+        set_pwm_angle(1, 0, 300);
 
         sleep(1);
 
-        set_pwm_angle(1, 90, 100);
+        set_pwm_angle(1, 90, 300);
 
         sleep(1);
 
-        set_pwm_angle(1, 180, 100);
+        set_pwm_angle(1, 180, 300);
 
         sleep(1);
     }
