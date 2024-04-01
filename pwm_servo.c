e78e320b7f05e46ea58ec92030409b04fc32e8a8
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-02 04:56:55 +0700

    fix

--- pwm_servo.c
+++ pwm_servo.c
@@ -86,7 +86,7 @@ void set_pwm_angle(uint8_t channel, int angle, int freq)
         angle = 180;
     }
 
-    int pulse_width = MIN_PULSE_WIDTH - (angle * 10);
+    int pulse_width = (angle - 0) * (MAX_PULSE_WIDTH - MIN_PULSE_WIDTH) / (ANGLE_RANGE - 0) + MIN_PULSE_WIDTH;
 
     set_pwm_duty(channel, pulse_width);
 
