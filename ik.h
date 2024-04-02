97a5eb284ea88370820ce57d9bbb8d505428d8e1
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-02 08:28:27 +0700

    fix

--- ik.h
+++ ik.h
@@ -8,7 +8,7 @@
 #define FEMUR_LENGTH 80.0
 #define TIBIA_LENGTH 170.0
 #define INITIAL_ANGLE2 30
-#define INITIAL_ANGLE3 30
+#define INITIAL_ANGLE3 0
 #define PI 3.141559265359
 
 void calculate_ik(float x, float y, float z, float* angles);
