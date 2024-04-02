21a26afc862f2813a8f7b0146f289cfa217a32d0
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-02 08:38:57 +0700

    fix

--- ik.c
+++ ik.c
@@ -8,27 +8,22 @@ void calculate_ik(float x, float y, float z, float* angles)
 
     float dist_xyz = sqrt(dist_xy*dist_xy + z*z);
 
-    float alpha = acos((FEMUR_LENGTH*FEMUR_LENGTH + TIBIA_LENGTH*TIBIA_LENGTH - dist_xyz*dist_xyz) / (2 * FEMUR_LENGTH *TIBIA_LENGTH));
+    if (dist_xyz > FEMUR_LENGTH + TIBIA_LENGTH || dist_xyz < fabs(FEMUR_LENGTH - TIBIA_LENGTH))
+    {
+        // Invalid position, set angles to initial values
+        angles[1] = INITIAL_ANGLE2;
+        angles[2] = INITIAL_ANGLE3;
+    }
+    else
+    {
+        float alpha = acos((FEMUR_LENGTH*FEMUR_LENGTH + TIBIA_LENGTH*TIBIA_LENGTH - dist_xyz*dist_xyz) / (2 * FEMUR_LENGTH * TIBIA_LENGTH));
+        float beta = acos((dist_xy*dist_xyz + FEMUR_LENGTH*FEMUR_LENGTH - TIBIA_LENGTH*TIBIA_LENGTH) / (2 * dist_xyz * FEMUR_LENGTH));
 
-    float beta = acos ((dist_xy*dist_xyz + FEMUR_LENGTH*FEMUR_LENGTH - TIBIA_LENGTH*TIBIA_LENGTH) / (2 * dist_xyz * FEMUR_LENGTH));
-
-    angles[1] = PI/2 - alpha - atan2(z, dist_xy);
-
-    angles[2] = PI - beta;
-
-    if (isnan(angles[1])) {
-        angles[1] = INITIAL_ANGLE2; // Replace INITIAL_ANGLE_2 with the initial angle for joint 2
+        angles[1] = PI/2 - alpha - atan2(z, dist_xy);
+        angles[2] = PI - beta;
     }
 
-    if (isnan(angles[2])) {
-        angles[2] = INITIAL_ANGLE3; // Replace INITIAL_ANGLE_3 with the initial angle for joint 3
-    }
-
-    if (isnan(angles[0])) {
-        angles[0] = INITIAL_ANGLE1;
-    }
-
-     // Print the calculated angles
+    // Print the calculated angles
     printf("Calculated Joint Angles:\n");
     printf("Angle 1: %f\n", angles[0]);
     printf("Angle 2: %f\n", angles[1]);
