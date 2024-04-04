081ea34281c93d20d1e0ca9f3428ec3653a690ee
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-04 20:09:55 +0700

    fix

--- main.c
+++ main.c
@@ -47,7 +47,7 @@ int main() {
     }
 
     // Get the target position (example value)
-    float target[3] = {110, 100, -30};
+    float target[3] = {110, 100, 100};
 
     // Perform inverse kinematics to calculate joint angles for the target position
     inverse_kinematics(&leg, target);
