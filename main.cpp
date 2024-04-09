16516cc4dc2112daf801671f68798dd3c4a62c91
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-10 01:15:02 +0700

    fix

--- main.cpp
+++ main.cpp
@@ -4,9 +4,13 @@ int main() {
 
     PCA9685_init();
      // Create a hexapod object with debug mode enabled for informative messages
-    set_pwm_angle(1, 90, 50);
-    sleep(1);
-    set_pwm_angle(1, 0, 50);
+    while(1){
+        set_pwm_angle(1, 90, 50);
+        sleep(1);
+        set_pwm_angle(1, 0, 50);
+        sleep(1);
+    }
+
 
   return 0;
 }
