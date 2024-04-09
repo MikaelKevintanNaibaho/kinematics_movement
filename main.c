6aa083d2ef30330aa10df918825aa60b4b9d100a
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-10 05:30:25 +0700

    fix

--- main.c
+++ main.c
@@ -18,19 +18,19 @@ int main(void){
         //lift the leg
         float target_lift[3] = {0.0, 0.0, lift_height};
         inverse_kinematics(&leg, target_lift);
-        sleep(0.5);
+        sleep(1);
         //move forward
         float target_forward[3] = {step_lenght, 0.0, lift_height};
         inverse_kinematics(&leg, target_forward);
-        sleep(0.5);
+        sleep(1);
         //palce down
         float target_down[3] = {step_lenght, 0.0, 0.0};
         inverse_kinematics(&leg, target_down);
-        sleep(0.5);
+        sleep(1);
         //move backward
         float target_backward[3] = {0.0, 0.0, 0.0};
         inverse_kinematics(&leg, target_backward);
-        sleep(0.5);
+        sleep(1);
     }
 
     return 0;
