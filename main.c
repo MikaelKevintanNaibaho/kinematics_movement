b92e043e18ad0dab139cd7dbc537d1fddd67aec3
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-10 19:38:57 +0700

    fix

--- main.c
+++ main.c
@@ -11,31 +11,31 @@ int main(void){
 
     sleep(2);
 
-    target[0] += 100;
+    // target[0] += 100;
 
-    inverse_kinematics(&leg, target);
-    // float lift_height = 20.0;
-    // float step_lenght = 50;
-    // float num_step = 3;
+    // inverse_kinematics(&leg, target);
+    float lift_height = 100.0;
+    float step_lenght = 50;
+    float num_step = 3;
 
-    // for (int i = 0; i < num_step; i++){
-    //     //lift the leg
-    //     float target_lift[3] = {0.0, 0.0, lift_height};
-    //     inverse_kinematics(&leg, target_lift);
-    //     sleep(1);
-    //     //move forward
-    //     float target_forward[3] = {step_lenght, 0.0, lift_height};
-    //     inverse_kinematics(&leg, target_forward);
-    //     sleep(1);
-    //     //palce down
-    //     float target_down[3] = {step_lenght, 0.0, 0.0};
-    //     inverse_kinematics(&leg, target_down);
-    //     sleep(1);
-    //     //move backward
-    //     float target_backward[3] = {0.0, 0.0, 0.0};
-    //     inverse_kinematics(&leg, target_backward);
-    //     sleep(1);
-    // }
+    for (int i = 0; i < num_step; i++){
+        //lift the leg
+        float target_lift[3] = {0.0, 0.0, lift_height};
+        inverse_kinematics(&leg, target_lift);
+        sleep(1);
+        //move forward
+        float target_forward[3] = {step_lenght, 0.0, lift_height};
+        inverse_kinematics(&leg, target_forward);
+        sleep(1);
+        //palce down
+        float target_down[3] = {step_lenght, 0.0, 0.0};
+        inverse_kinematics(&leg, target_down);
+        sleep(1);
+        //move backward
+        float target_backward[3] = {0.0, 0.0, 0.0};
+        inverse_kinematics(&leg, target_backward);
+        sleep(1);
+    }
 
     return 0;
 }
