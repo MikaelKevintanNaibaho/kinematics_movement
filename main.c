bf93ea4c0ab5de4df3214046bb08cf2959e0e42e
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-13 07:15:47 +0700

    fix

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int main(void) {
     PCA9685_init();
 
     SpiderLeg leg;
-    float initial_angle[3] = {0, 130, 130};
+    float initial_angle[3] = {0, 100, 130};
     set_angles(&leg, initial_angle);
     forward_kinematics(&leg, initial_angle);
     printf("x = %.2f, y = %.2f, z = %.2f\n", leg.joints[3][0], leg.joints[3][1], leg.joints[3][2]); 
