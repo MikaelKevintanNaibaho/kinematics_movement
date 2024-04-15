212b1c4d6cdd5117954b9b014beef612242abd1c
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-16 05:09:48 +0700

    fix

--- main.c
+++ main.c
@@ -9,13 +9,13 @@ int main(void) {
     for (int i = 0; i < NUM_LINKS; i++) {
         intermediate_link[i] = gsl_matrix_alloc(4, 4);
     }
-    float initial_angle[3] = {0, 130, 130};
+    float initial_angle[3] = {45, 130, 130};
     set_angles(&leg, initial_angle);
 
     sleep(2);
     forward_kinematics(&leg, initial_angle, intermediate_link); // Removed the '&' before intermediate_link
 
-    float target_positions[3] = {0.0, -50.0, 0.0};
+    float target_positions[3] = {-20.0, 0.0, 0.0};
 
     inverse_kinematics(&leg, target_positions, intermediate_link);
     return 0;
