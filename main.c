f3760737f3691182f6720d899ee2cb1f2295ddf2
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-19 03:38:26 +0700

    fix

--- main.c
+++ main.c
@@ -16,8 +16,6 @@ int main(void) {
     sleep(2);
     forward_kinematics(&leg, initial_angle, intermediate_link); // Removed the '&' before intermediate_link
 
-    // Move the leg to the target angles
-    // move_to_angle(&leg, target_angles, 100);
     struct bezier2d curve;
     bezier2d_init(&curve);
 
