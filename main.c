b666b2fb1cc6d308ea45edb6526e5787ce9d4a1b
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-19 21:27:28 +0700

    fix

--- main.c
+++ main.c
@@ -16,24 +16,24 @@ int main(void) {
     sleep(2);
     forward_kinematics(&leg, initial_angle, intermediate_link); // Removed the '&' before intermediate_link
 
-    float target[3] = {-50.0, 0.0, 0.0};
-    inverse_kinematics(&leg, target, intermediate_link);
+    // float target[3] = {-50.0, 0.0, 0.0};
+    // inverse_kinematics(&leg, target, intermediate_link);
 
     // float angle[3] = {0, 90, 90};
     // move_to_angle(&leg, angle, 100);
 
-    // struct bezier2d curve;
-    // bezier2d_init(&curve);
+    struct bezier2d curve;
+    bezier2d_init(&curve);
 
-    // float stride_length = 50.0;
-    // float swing_high = 50.0;
-    // generate_walk_trajectory(&curve, &leg, stride_length, swing_high);
+    float stride_length = 50.0;
+    float swing_high = 50.0;
+    generate_walk_trajectory(&curve, &leg, stride_length, swing_high);
 
-    // save_trajectory_points(&curve, "trajectory.dat", 50);
+    save_trajectory_points(&curve, "trajectory.dat", 50);
 
 
-    // int num_points = 200.0;
-    // update_leg_position_with_velocity(&curve, num_points, &leg, intermediate_link);
+    int num_points = 200.0;
+    update_leg_position_with_velocity(&curve, num_points, &leg, intermediate_link);
 
      for (int i = 0; i < NUM_LINKS; i++) {
         gsl_matrix_free(intermediate_link[i]);
