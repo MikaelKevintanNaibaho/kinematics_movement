2427f9ba9cf2fe93942529bdb96e3bedef0a947b
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-21 05:01:12 +0700

    fix

--- move.h
+++ move.h
@@ -21,7 +21,7 @@ struct bezier2d {
 };
 
 #define NUM_POINTS 60
-#define DESIRED_TIME 10
+#define DESIRED_TIME 0.1
 
 void bezier2d_init(struct bezier2d *curve);
 void bezier2d_addPoint(struct bezier2d *curve, float x, float y);
