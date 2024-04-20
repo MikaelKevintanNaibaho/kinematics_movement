ad1fa3fdc840b6261246600c01a68c6ccc331b90
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-20 17:54:53 +0700

    fix

--- move.c
+++ move.c
@@ -65,7 +65,7 @@ void generate_walk_trajectory(struct bezier2d *curve, SpiderLeg *leg, float stri
     //control points
     float controlx = startx + stride_length;
     float controlz = startz + swing_height;
-    float endx_forward = startx + (2 * stride_length);
+    float endx_forward = startx + stride_length;
     float endz_forward = startz;
     //buar bezier curve
     bezier2d_generate_curve(curve, startx, startz, controlx, controlz, endx_forward, endz_forward);
