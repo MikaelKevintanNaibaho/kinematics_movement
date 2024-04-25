69cdaa83fd5fda15a9d07c19a6f33034af959aca
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-25 09:42:27 +0700

    fix

--- ik.c
+++ ik.c
@@ -298,10 +298,6 @@ void inverse_kinematics(SpiderLeg *leg, float target_positions[3], LegPosition p
     theta2 = degrees(theta2);
     theta3 = degrees(theta3);
 
-    if (theta1 > 90){
-        theta1 = 180.0 - theta1;
-    }
-
     // if (position_leg == KANAN_BELAKANG || position_leg == KIRI_BELAKANG) {
     //     theta1 += 90.0;
     // }
