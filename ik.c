d6dcd4af1f36e4ba2ce2351e41664eaa03e033d5
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-29 02:33:36 +0700

    fix

--- ik.c
+++ ik.c
@@ -362,8 +362,8 @@ void adjust_coordinate(float x, float y, float z, LegPosition position, float *a
         *adj_z = z;
         break;
     case KIRI_BELAKANG:
-        *adj_x = -y;
-        *adj_y = -x;
+        *adj_x = -x;
+        *adj_y = -y;
         *adj_z = z;
         break;
     case KIRI_DEPAN:
