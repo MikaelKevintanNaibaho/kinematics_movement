5bca535485a3d295195087abaff6b8f39afc68b1
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-04-28 21:39:28 +0700

    fix

--- ik.c
+++ ik.c
@@ -188,13 +188,13 @@ void forward_kinematics(SpiderLeg *leg, float angles[3], LegPosition position_le
         zero_offset = 0.0;
         break;
     case KIRI_DEPAN:
-        zero_offset = 90.0;
+        zero_offset = 0.0;
         break;
     case KIRI_BELAKANG:
-        zero_offset = 180.0;
+        zero_offset = 0.0;
         break;
     case KANAN_BELAKANG:
-        zero_offset = 90.0;
+        zero_offset = 0.0;
     default:
         break;
     }
