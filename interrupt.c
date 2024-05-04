61c58d4479ac023c0f12789b5e5cc195cb2c2086
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-05 05:59:08 +0700

    fix

--- interrupt.c
+++ interrupt.c
@@ -4,7 +4,7 @@
 int is_program_running = 0;
 void switch_interrupt(void)
 {
-    if (digitalRead(SWITCH_PIN) == HIGH) {
+    if (digitalRead(SWITCH_PIN) == LOW) {
         if (is_program_running) {
             stop_program();
         } else {
@@ -17,7 +17,7 @@ void init_interrupt(void)
 {
     wiringPiSetup();
     pinMode(SWITCH_PIN, INPUT);
-    pullUpDnControl(SWITCH_PIN, PUD_DOWN);
+    pullUpDnControl(SWITCH_PIN, PUD_UP);
     wiringPiISR(SWITCH_PIN, INT_EDGE_FALLING, &switch_interrupt);
 }
 
