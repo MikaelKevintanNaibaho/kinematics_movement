76b526b9fd986f0cbb06e5839bfb6e9bbc5d9237
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-26 03:01:24 +0700

    fix

--- masang_servo.c
+++ masang_servo.c
@@ -3,10 +3,18 @@
 #include "pwm_servo.h"
 #include "ik.h"
 
-#define PWM_FREQ 50
 #define SERVO_CHANNEL_1 1
 #define SERVO_CHANNEL_2 2
 #define SERVO_CHANNEL_3 3
+#define SERVO_CHANNEL_4 4
+#define SERVO_CHANNEL_5 5
+#define SERVO_CHANNEL_6 6
+#define SERVO_CHANNEL_7 7
+#define SERVO_CHANNEL_8 8
+#define SERVO_CHANNEL_9 9
+#define SERVO_CHANNEL_10 10
+#define SERVO_CHANNEL_11 11
+#define SERVO_CHANNEL_12 12
 
 int main(void)
 {
