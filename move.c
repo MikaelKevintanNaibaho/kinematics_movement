ff9d9df8719e34eb32f07175b365640fbc385570
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-25 07:18:13 +0700

    fix

--- move.c
+++ move.c
@@ -4,7 +4,7 @@ void generate_walk_trajectory(struct bezier2d *curve, SpiderLeg *leg, float stri
                               float swing_height, LegPosition position_leg)
 {
     // get current position
-    float startx = leg->joints[3][0];
+    float startx = leg->joints[3][0] - stride_length;
     float startz = leg->joints[3][2];
     printf("startx = %f, startz %f\n", startx, startz);
 
