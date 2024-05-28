c489fe45febf278abd4f93e76cf298e4c91bfdcd
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-29 05:19:59 +0700

    fix

--- move.h
+++ move.h
@@ -30,8 +30,8 @@ struct LegThreadData
     LegPosition position_leg;
 };
 
-#define STRIDE_LENGTH 120.0
-#define SWING_HEIGHT 50.0
+#define STRIDE_LENGTH 70.0
+#define SWING_HEIGHT 70.0
 #define NUM_POINTS 30.0
 #define DESIRED_TIME 0.00001
 #define GROUP_SIZE 2
