b9c38b020155700935671d079fbf4b3be560210a
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-05-28 07:18:47 +0700

    fix

--- move.c
+++ move.c
@@ -98,7 +98,7 @@ void generate_turn_left_trajectory(struct bezier3d *curve, SpiderLeg *leg, float
     float controlz = startz + 2 * swing_height;
 
     float endx = startx;
-    float endy = starty + stride_length;
+    float endy = starty +  2 * stride_length;
     float endz = startz;
 
     bezier3d_generate_curve(curve, startx, starty, startz, controlx, controly, controlz, endx, endy,
