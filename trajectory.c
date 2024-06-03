159cb1c56414a89729b2bdfc99bde910d2f800e3
MikaelKevintanNaibaho <mikaelkevintannaibaho@gmail.com>
2024-06-04 03:49:19 +0700

    fix: the trajectory

--- trajectory.c
+++ trajectory.c
@@ -1,22 +1,37 @@
 #include "trajectory.h"
 
 //fungsi untuk menciptakan trajectory swing ke depan
-void generate_swing_phase(struct bezier2d *curve, float startx, float startz, float stride_length, float swing_height)
+void generate_swing_phase(struct bezier2d *curve, float startx, float startz, float stride_length, float swing_height, int leg_type)
 {
-    float controlx = startx + stride_length / 2;
+    float controlx = 0;
     /*ditambahkan dari posisi awal z karena posisi awal adalah negative(-), jadi agar mengangkat kaki maka ketinggian harus dikurangi*/
     float controlz = startz + 2 * swing_height;
-    float endx = startx + stride_length;
+
+    float endx = 0;
+    if (leg_type == 1) {
+        controlx = startx + stride_length / 2;
+        endx = startx + stride_length;
+    } else if (leg_type == -1) {
+        controlx = startx - stride_length / 2;
+        endx = startx - stride_length;
+    }
     float endz = startz;
 
     bezier2d_generate_curve(curve, startx, startz, controlx, controlz, endx, endz);
 }
 
-void generate_stance_phase(struct bezier2d *curve, float startx, float startz, float stride_lenght)
+void generate_stance_phase(struct bezier2d *curve, float startx, float startz, float stride_lenght, int leg_type)
 {
-    float controlx = startx - stride_lenght / 2;
+    float controlx = 0;
     float controlz = startz - SWING_PUSH_BACK;
-    float endx = startx - stride_lenght;
+    float endx = 0;
+    if (leg_type == 1) {
+        controlx = startx - stride_lenght / 2;
+        endx = startx - stride_lenght;
+    } else if (leg_type == -1) {
+        controlx = startx + stride_lenght / 2;
+        endx = startx + stride_lenght;
+    }
     float endz = startz;
 
     bezier2d_generate_curve(curve, startx, startz, controlx, controlz, endx, endz);
@@ -26,17 +41,17 @@ void generate_stance_phase(struct bezier2d *curve, float startx, float startz, f
 void generate_walk_trajectory(struct bezier2d *curve, SpiderLeg *leg, float stride_length, float swing_height, LegPosition leg_positions)
 {
     //get current position
-    float startx_swing = leg->joints[3][0];
+    float startx_swing = leg->joints[3][0] - stride_length;
     float startz_swing = leg->joints[3][2];
 
     //buat swing phase
-    generate_swing_phase(curve, startx_swing, startz_swing, stride_length, swing_height);
+    generate_swing_phase(curve, startx_swing, startz_swing, stride_length, swing_height, FRONT);
 
     //update start position untuk stance phase
-    float startx_stance = startx_stance + stride_length;
+    float startx_stance = startx_swing + stride_length;
     float startz_stance = startz_swing;
 
-    generate_stance_phase(curve, startx_stance, startz_stance, stride_length);
+    generate_stance_phase(curve, startx_stance, startz_stance, stride_length, FRONT);
 
 }
 
@@ -48,12 +63,12 @@ void generate_walk_back_leg_trajectory(struct bezier2d *curve, SpiderLeg *leg, f
     float startz_swing = leg->joints[3][2];
 
     //buat swing phase
-    generate_swing_phase(curve, startx_swing, startz_swing, stride_length, swing_height);
+    generate_swing_phase(curve, startx_swing, startz_swing, stride_length, swing_height, BACK);
 
     //update start position untuk stance phase
-    float startx_stance = startx_stance - stride_length;
+    float startx_stance = startx_swing - stride_length;
     float startz_stance = startz_swing;
 
-    generate_stance_phase(curve, startx_stance, startz_stance, stride_length);
+    generate_stance_phase(curve, startx_stance, startz_stance, stride_length, BACK);
 
 }
--- trajectory.h
+++ trajectory.h
@@ -5,9 +5,11 @@
 #include "leg.h"
 
 #define SWING_PUSH_BACK 15
+#define FRONT 1
+#define BACK -1
 
-void generate_swing_phase(struct bezier2d *curve, float startx, float startz, float stride_length, float swing_height);
-void generate_stance_phase(struct bezier2d *curve, float startx, float startz, float stride_leght);
+void generate_swing_phase(struct bezier2d *curve, float startx, float startz, float stride_length, float swing_height, int leg_type);
+void generate_stance_phase(struct bezier2d *curve, float startx, float startz, float stride_leght, int leg_type);
 void generate_walk_trajectory(struct bezier2d *curve, SpiderLeg *leg, float stride_length, float swing_height, LegPosition leg_positions);
 void generate_walk_back_leg_trajectory(struct bezier2d *curve, SpiderLeg *leg, float stride_length, float swing_height, LegPosition leg_positions);
 
